db7355306df22657749c69e8efed349cf61adaf5
k5jm <118034837+k5jmreal@users.noreply.github.com>
2022-11-12 03:37:38 +0000

    Encoded video

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import encoder 
 
 def main():
-    encoder.encode("RatatouilleEnding","testvideo.mp4",.1,.1,10)
+    encoder.encode("video","testvideo.mp4",.1,.1,10)
 
 if __name__== "__main__":
     main()
